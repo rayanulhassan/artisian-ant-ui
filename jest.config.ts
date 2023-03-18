5acbd8a380128582540bc52e42b230d9e64543e8
hassan_raza <rayanulhassan@outlook.com>
2023-03-18 16:09:24 +0500

    initial commit

--- jest.config.ts
+++ jest.config.ts
@@ -1,26 +1,5 @@
-/* eslint-disable */
+import { getJestProjects } from '@nrwl/jest';
+
 export default {
-  displayName: 'artisian-ant-ui',
-  preset: './jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: './coverage/artisian-ant-ui',
-  transform: {
-    '^.+\\.(ts|mjs|js|html)$': [
-      'jest-preset-angular',
-      {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
-        stringifyContentPathRegex: '\\.(html|svg)$',
-      },
-    ],
-  },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
-  snapshotSerializers: [
-    'jest-preset-angular/build/serializers/no-ng-attributes',
-    'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment',
-  ],
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
-    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
-  ],
+  projects: getJestProjects(),
 };
